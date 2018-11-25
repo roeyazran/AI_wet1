cdfd879a69a6e5456bc9c29156c16ca6c78d3317
shlomid <shlomo.dabi@campus.technion.ac.il>
2018-11-25 21:39:22 +0200

    calculating probability as instructed

--- experiments/temperature.py
+++ experiments/temperature.py
@@ -2,14 +2,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 X = np.array([400, 450, 900, 390, 550])
-
 # TODO: Write the code as explained in the instructions
-raise NotImplemented()  # TODO: remove!
+T = np.linspace(0.01, 5.0, num=100)
+P = []
+for x in X:
+    tRow=[]
+    for t in T:
+        denominator=0
+        for x_mechane in X:
+            denominator += (x_mechane**(-1/t))
+        tRow.append((x**(-1/t))/denominator)
+    P.append(tRow)
 
 print(P)
 
 for i in range(len(X)):
-    plt.plot(T, P[:, i], label=str(X[i]))
+    plt.plot(T, P[i], label=str(X[i]))
 
 plt.xlabel("T")
 plt.ylabel("P")
